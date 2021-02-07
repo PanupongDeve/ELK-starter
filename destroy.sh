96b9d75cd222c8a0a660697e1a54312431b3b6e0
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-02-07 04:55:00 +0000

    add logstash

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,5 @@
 docker-compose -f ./kibana/docker-compose.yaml down
+docker-compose -f ./logstash/docker-compose.yaml down
 docker-compose -f ./nginx/docker-compose.yaml down
 docker-compose -f ./debugger/docker-compose.yaml down
 docker-compose -f ./elasticsearch/docker-compose.yaml down
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 docker-compose -f ./elasticsearch/docker-compose.yaml up -d
+docker-compose -f ./logstash/docker-compose.yaml up -d
 docker-compose -f ./kibana/docker-compose.yaml up -d
 docker-compose -f ./nginx/docker-compose.yaml up -d
 docker-compose -f ./debugger/docker-compose.yaml up -d
